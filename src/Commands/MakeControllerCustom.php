11143ee3aef925c413e49cd6e3dc724f014871cf
Nurdin <nurdin4f@gmail.com>
2025-05-06 20:24:40 +0700

    controllerku

--- src/Commands/MakeControllerCustom.php
+++ src/Commands/MakeControllerCustom.php
@@ -7,46 +7,47 @@ use Illuminate\Support\Facades\File;
 
 class MakeControllerCustom extends Command
 {
-    protected $signature = 'make:controllerku {name}';
-    protected $description = 'Buat File Controller Custom dengan stub';
+    protected $signature    = 'make:controllerku {name}';
+    protected $description  = 'Buat File Controller Custom dengan stub';
 
     public function handle()
     {
-        $name = str_replace('/', '\\', $this->argument('name'));
-        $controllerPath = app_path("Http/Controllers/{$name}.php");
-
+        $argumentName = $this->argument('name'); // tetap pakai slash sebagai pemisah folder
+    
+        $controllerPath = app_path('Http/Controllers/' . $argumentName . '.php');
+    
         $customStubPath = base_path("stubs/controllerku.stub");
         $defaultStubPath = __DIR__ . '/../../stubs/controllerku.stub';
         $stubPath = File::exists($customStubPath) ? $customStubPath : $defaultStubPath;
-
+    
         if (!File::exists($stubPath)) {
             $this->error("File stub tidak ditemukan di: {$stubPath}");
             return Command::FAILURE;
         }
-
+    
         if (File::exists($controllerPath)) {
-            $this->error("Controller [{$name}] sudah ada!");
+            $this->error("Controller [{$argumentName}] sudah ada!");
             return Command::FAILURE;
         }
-
+    
         $stub = File::get($stubPath);
-
-        // Ambil class name dan namespace dari input
-        $className = class_basename($name);
-        $namespace = "App\Http\Controllers" . (str_contains($name, '\\') ? '\\' . dirname(str_replace('\\', '/', $name)) : '');
-
-        $namespace = str_replace('/', '\\', trim($namespace, '\\'));
-
+    
+        $className = class_basename($argumentName);
+        $subNamespace = trim(str_replace('/', '\\', dirname($argumentName)), '\\');
+    
+        $namespace = 'App\Http\Controllers' . ($subNamespace ? '\\' . $subNamespace : '');
+    
         $content = str_replace(
             ['{{ namespace }}', '{{ class }}'],
             [$namespace, $className],
             $stub
         );
-
+    
         File::ensureDirectoryExists(dirname($controllerPath));
         File::put($controllerPath, $content);
-
-        $this->info("Controller [{$name}] berhasil dibuat 🎉");
+    
+        $this->info("Controller [{$argumentName}] berhasil dibuat 🎉");
         return Command::SUCCESS;
     }
+    
 }
